e47183ffb45a5ceaea0058219338b574ff0d736a
Shahalam Sharif <shahalamsharif000@gmail.com>
2023-06-24 10:22:06 +0800

    authentication completed

---
+++ migrations/20230623141606-create-auth.js
@@ -0,0 +1,49 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Auths", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      full_name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      verified: {
+        type: Sequelize.BOOLEAN,
+      },
+      account_status: {
+        type: Sequelize.STRING,
+      },
+      role: {
+        type: Sequelize.INTEGER,
+      },
+      token: {
+        type: Sequelize.STRING,
+      },
+      profile_ref: {
+        type: Sequelize.UUID,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Auths");
+  },
+};
---
+++ routes/v1/auth.routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  signUp,
+  logIn,
+  verify,
+  verifyOTP,
+  forgotPassword,
+  resetPassword,
+  verifyEmail,
+} = require("../../controllers/auth.controllers");
+
+router.post("/register", signUp);
+router.post("/login", logIn);
+router.get("/verify_email/:token", verifyEmail);
+router.post("/forgot_password", forgotPassword);
+router.post("/reset_password", resetPassword);
+
+module.exports = router;
