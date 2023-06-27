95ee37107eb6f43ec5f0ef6546fdd94e81bc764f
Shahalam Sharif <shahalamsharif000@gmail.com>
2023-06-27 19:11:59 +0800

    verify email url updated

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -33,7 +33,7 @@ const signUp = (req, res, next) => {
 
         const { token, generateLink } = await generateOtpLink(
           email,
-          `${process.env.SERVER_URL}/auth/verify_email/`
+          `${process.env.SERVER_URL}/verify_email/`
         );
 
         user.token = token;
